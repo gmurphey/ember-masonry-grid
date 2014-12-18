09c5acb9cae312c8ce0b0d4b1e2e8f78aee3573d
Garrett Murphey <gmurphey@gmurphey.com>
2014-12-18 15:17:22 -0500

    Adding import for masonry.

--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-masonry-grid'
+  name: 'ember-masonry-grid',
+
+  included: function (app) {
+    this._super.included(app);
+
+    app.import(app.bowerDirectory + '/masonry/dist/masonry.pkgd.min.js');
+  }
 };

fe5c847fa1d1539a87a80130e45543c62d3bb7cb
Will Viles <will@willviles.com>
2017-08-01 15:21:07 +0100

    Adds Fastboot 1.0.0 compatibility

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 /* eslint-env node */
 'use strict';
 
-var Funnel = require('broccoli-funnel');
-var mergeTrees = require('broccoli-merge-trees');
-var path = require('path');
+const Funnel = require('broccoli-funnel');
+const mergeTrees = require('broccoli-merge-trees');
+const fastbootTransform = require('fastboot-transform');
+const path = require('path');
 
 module.exports = {
   name: 'ember-masonry-grid',
@@ -12,48 +13,53 @@ module.exports = {
 
     this.addonConfig = this.app.project.config(app.env)['ember-masonry-grid'] || {};
 
-    const vendor = this.treePaths.vendor;
+    let { treePaths: { vendor } } = this;
 
-    if (!isFastBoot()) {
-      // Masonry
+    // Masonry
+    app.import({
+      development: `${vendor}/masonry/masonry.pkgd.js`,
+      production: `${vendor}/masonry/masonry.pkgd.min.js`
+    });
+
+    // Imagesloaded
+    if (this.addonConfig.imagesLoaded !== false) {
       app.import({
-        development: vendor + '/masonry/masonry.pkgd.js',
-        production: vendor + '/masonry/masonry.pkgd.min.js'
+        development: `${vendor}/imagesloaded/imagesloaded.pkgd.js`,
+        production: `${vendor}/imagesloaded/imagesloaded.pkgd.min.js`
       });
-
-      // Imagesloaded
-      if (this.addonConfig.imagesLoaded !== false) {
-        app.import({
-          development: vendor + '/imagesloaded/imagesloaded.pkgd.js',
-          production: vendor + '/imagesloaded/imagesloaded.pkgd.min.js'
-        });
-      }
-
     }
 
     return this._super.included.apply(this, arguments);
   },
 
   treeForVendor(vendorTree) {
-    var trees = [];
+    let trees = [];
 
     if (vendorTree) {
       trees.push(vendorTree);
     }
 
-    trees.push(moduleToFunnel('masonry-layout', {
-      srcDir: 'dist',
-      include: ['*.pkgd.js', '*.pkgd.min.js'],
-      destDir: 'masonry'
-    }));
+    trees.push(
+      fastbootTransform(
+        moduleToFunnel('masonry-layout', {
+          srcDir: 'dist',
+          include: ['*.pkgd.js', '*.pkgd.min.js'],
+          destDir: 'masonry'
+        })
+      )
+    );
 
-    trees.push(moduleToFunnel('imagesloaded', {
-      include: ['*.pkgd.js', '*.pkgd.min.js'],
-      destDir: 'imagesloaded'
-    }));
+    trees.push(
+      fastbootTransform(
+        moduleToFunnel('imagesloaded', {
+          include: ['*.pkgd.js', '*.pkgd.min.js'],
+          destDir: 'imagesloaded'
+        })
+      )
+    );
 
     return mergeTrees(trees);
-  },
+  }
 
 };
 
@@ -64,11 +70,4 @@ function moduleToFunnel(moduleName, opts) {
 
 function resolveModulePath(moduleName) {
   return path.dirname(require.resolve(moduleName));
-}
-
-// Checks to see whether this build is targeting FastBoot. Note that we cannot
-// check this at boot time--the environment variable is only set once the build
-// has started, which happens after this file is evaluated.
-function isFastBoot() {
-  return process.env.EMBER_CLI_FASTBOOT === 'true';
 }
